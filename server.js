df796287cbb9f9660301087b7ac1c0dd9bfdd930
Amanda Rost <amandarostprog@gmail.com>
2025-03-23 11:28:18 -0300

    Correção vercel.json

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 require('dotenv').config();
-console.log(process.env.CONNECTION_STRING);
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(bodyParser.json());
+app.use(cors()); // Enable CORS for all routes
 
 const CONNECTION_STRING = process.env.CONNECTION_STRING;
 
