a0cff7795601bb3097a002d9592feddb591c9e72
Amanda Rost <amandarostprog@gmail.com>
2025-06-10 22:19:25 -0300

    Reestrutura db

--- src/db.js
+++ src/db.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI);
+    await mongoose.connect(process.env.CONNECTION_STRING);
     console.log('Conectado com o MongoDB Atlas com sucesso :D');
   } catch (error) {
     console.error('Falha em conectar com MongoDB Atlas :(', error.message);
